32828f6b3a1454f6803ec1f57f1cc052f6e27075
hyuk0309 <bahngeh@naver.com>
2022-12-17 01:05:23 +0900

    feat : edit write.html and implement post api

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.urlencoded({extended: true})) 
 
 app.listen(8080, function() {
     console.log('start server... port : 8080')
@@ -19,4 +20,9 @@ app.get('/', function(req, res) {
 
 app.get('/write', function(req, res) {
     res.sendFile(__dirname + '/write.html')
+})
+
+app.post('/add', function(req, res) {
+    console.log(req.body)
+    res.send("전송완료")
 })
