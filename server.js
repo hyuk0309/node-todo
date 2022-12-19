7a37555f0fa91074f0e3789a343897f324912fee
hyuk0309 <bahngeh@naver.com>
2022-12-20 00:03:36 +0900

    feat : insert data to mongodb

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ app.use(express.urlencoded({extended: true}))
 
 const MongoClient = require('mongodb').MongoClient;
 
+var db
 MongoClient.connect('mongodb+srv://admin:admin123@cluster0.y6p2d0u.mongodb.net/?retryWrites=true&w=majority', function(err, client) {
-    if (err) return console.log(err);
-    //서버띄우는 코드 여기로 옮기기
+    if (err) return console.log(err)
+    db = client.db('todoapp')
+
     app.listen('8080', function(){
       console.log('listening on 8080')
     });
@@ -29,6 +31,11 @@ app.get('/write', function(req, res) {
 })
 
 app.post('/add', function(req, res) {
+
+    db.collection('post').insertOne( { 제목 : req.body.title, 날짜 : req.body.date} , function(err, result) {
+	    console.log('저장완료'); 
+	});
+
     console.log(req.body)
     res.send("전송완료")
 })
